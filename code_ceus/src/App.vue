48168c692f18b807ce880c557a7961194017ea53
Ales Dusenok <ales.dusenok@noveogroup.com>
2022-11-07 19:34:35 +0300

    basic_input

--- code_ceus/src/App.vue
+++ code_ceus/src/App.vue
@@ -2,6 +2,10 @@
 import { RouterLink, RouterView } from "vue-router";
 import Header from '../src/components/markup/Header.vue'
 import Menu from '../src/components/markup/Menu.vue'
+import {ref} from 'vue'
+import BasicInput from '../src/components/basic/Input.vue'
+
+const inputText = ref('zalupa')
 </script>
 
 <template>
@@ -9,6 +13,7 @@ import Menu from '../src/components/markup/Menu.vue'
   <div class="flex justify-between">
     <Menu></Menu>
     <div class="pt-12 pl-96 h-screen w-full"><div class="p-4"><RouterView /></div></div>
+    <div class="fixed z-90 top-20 right-0 w-96 h-96 bg-red-500 color-white">text:{{inputText}}<BasicInput v-model="inputText"></BasicInput></div>
   </div>
  
 </template>
---
+++ code_ceus/src/components/basic/Input.vue
@@ -0,0 +1,15 @@
+<template>
+    <div><input :value="props.modelValue" @input="updateValue($event.target.value)" type="text"></div>
+</template>
+
+<script setup>
+const props = defineProps({
+  modelValue: String
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+function updateValue(value) {
+  emit('update:modelValue', value)
+}
+</script>
