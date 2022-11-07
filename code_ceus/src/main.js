520d591fcd2cf205357fa7b220f7ed32fb684244
Ales Dusenok <ales.dusenok@noveogroup.com>
2022-11-07 10:36:00 +0300

    tailwind

--- code_ceus/src/main.js
+++ code_ceus/src/main.js
@@ -4,7 +4,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import "./assets/reset.css";
+import "./assets/tail.css";
 
 const app = createApp(App)
 
